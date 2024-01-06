f338a5c9134263083044751789b650ef0153dca3
tikitiki2 <108548646+tikitiki2@users.noreply.github.com>
2024-01-06 14:49:39 -0500

    Update helpers.h

--- helpers.h
+++ helpers.h
@@ -1,4 +1,5 @@
 void process_command(char []);
 bool validate_proccess_cmd(char,bool*,bool*);
+//convert an int num into int base
 int base10(int num,int base);
 int proccesschars(char temp[],int*,int );
